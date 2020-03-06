dfc974de28725e3a0c9e74b9a0abce0825093c99
Daniel Hongel <fatherofdharma@gmail.com>
2020-03-06 14:58:46 -0800

    add logic to make complete game

--- PingPong.cs
+++ PingPong.cs
@@ -32,6 +32,37 @@ class PingPong
     string number = Console.ReadLine();
     int yourNumber = int.Parse(number);
     Console.WriteLine("You entered " + yourNumber);
-
+    //A user enters a number and the application returns all numbers from 1 to the user input with the following changes:
+    // All numbers divisible by 3 are replaced by "ping".
+    // All numbers divisible by 5 are replaced by "pong"
+    // All numbers divisible by both 3 and 5 are replaced by "ping-pong".
+    if (yourNumber > 0)
+    {
+      for (int counter = 1; counter <= yourNumber; counter++)
+      {
+        if (counter % 3 == 0 && counter % 5 == 0)
+        {
+          Console.WriteLine("ping-pong");
+        }
+        else if (counter % 3 == 0)
+        {
+          Console.WriteLine("ping");
+        }
+        else if (counter % 5 == 0)
+        {
+          Console.WriteLine("pong");
+        }
+        else
+        {
+          Console.WriteLine(counter);
+        }
+      }
+      Main();
+    }
+    else
+    {
+      Console.WriteLine("That number is invalid, please enter a number greater than zero.");
+      PlayPingPong();
+    }
   }
 }
